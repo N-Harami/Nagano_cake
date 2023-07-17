27abfc76428701c716929a4ca0474c3405fbd463
EC2 Default User <ec2-user@ip-172-31-14-101.ap-northeast-1.compute.internal>
2023-07-17 10:22:51 +0000

    device_install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
