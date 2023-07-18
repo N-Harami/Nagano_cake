9d38d102abf16ad3630046c1e79d01384da95931
EC2 Default User <ec2-user@ip-172-31-14-101.ap-northeast-1.compute.internal>
2023-07-18 08:17:33 +0000

    配送先登録

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,14 @@ Rails.application.routes.draw do
   namespace :public do
     get 'cart_items/index'
   end
+
+  namespace :public do
+    get 'addresses' => 'addresses#index'
+    get 'addresses/edit'
+    post 'addresses' => 'addresses#create'
+  end
+
+
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
