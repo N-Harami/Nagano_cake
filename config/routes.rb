a3f92f0e13728e15f45b74989af7c31e9a6bd5bc
tencoco <135337078+tencoco@users.noreply.github.com>
2023-07-23 04:41:17 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -30,9 +30,19 @@ end
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  devise_for :admin, skip: [:registrations; :passwords], controllers: {
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+
+  namespace :admin do
+    get 'homes/top'
+    resources :itemes, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
