b6d17fd2eb40b266b096f74d676d39dc8b5c4978
EC2 Default User <ec2-user@ip-172-31-22-43.ap-northeast-1.compute.internal>
2023-07-24 05:28:50 +0000

    seeds　追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,12 @@
 class Item < ApplicationRecord
   has_one_attached :image
-  
-  has_many :orders, through: order_details
+
   has_many :order_details, dependent: :destroy
-  has_many :customers, through: cart_items
+  has_many :orders, through: :order_details
+  has_many :customers, through: :cart_items
   has_many :cart_items, dependent: :destroy
+
+
 
   belongs_to :genre, optional: true
   
