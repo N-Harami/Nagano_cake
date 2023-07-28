06dacb04a5dbf5e2d6366726d7274790c824a032
tencoco <135337078+tencoco@users.noreply.github.com>
2023-07-28 03:46:00 +0000

    add

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  before_action :reject_withdraw_customer, only: [:create]
+  before_action :is_quit_status, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -25,13 +25,12 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-  def reject_withdraw_customer
+  def is_quit_status
     @customer = Customer.find_by(email: params[:customer][:email].downcase)
-    if @customer
-      if (@customer.valid_password?(params[:customer][:password]) && (@customer.active_for_authentication? == false))
-        flash[:notice] = "退会済みのためログインできません。"
-        redirect_to new_customer_session_path
-      end
+    return if !@customer
+    if @customer.valid_password?(params[:customer][:password]) && @customer.is_deleted
+      flash[:notice] = "退会済みのためログインできません。"
+      redirect_to new_customer_session_path
     end
   end
 end
